735c52f8325938e2bb6308db3f85b22082481d21
awadhesh14 <dapst8@gmail.com>
2019-03-02 01:44:34 +0530

    added readme

--- assignment-2/Sequential-SSSP/sssp.c
+++ assignment-2/Sequential-SSSP/sssp.c
@@ -2,29 +2,20 @@
 #include "read_matrix2.h"
 
 int main(void){
-  int M, N, nz;
-  int i, *I, *J;
-  double *val;
-
-
-  // read_V_E("../input/USA-road-d.CAL.gr",&M,&nz);
-  // printf("%d %d \n",M,nz);
+  int M,nz;
+  int i;
 
   struct Edge *edges;// = new Edge[nz+1];
-  edges = read_edges("../input/USA-road-d.CAL.gr",&M,&nz);
-  // edges = read_edges("../input/test.input",&M,&nz);
+  // edges = read_edges("../input/USA-road-d.CAL.gr",&M,&nz);
+  edges = read_edges("../input/test.input",&M,&nz);
   double *dist = new double[M];
-  // std::memset (dist,std::numeric_limits<double>::infinity(),sizeof dist);
+
   for(i=0;i<M;i++){
     dist[i]=DBL_MAX;
-    // fprintf(stdout, "%lg\n", dist[i]);
   }
   dist[0] = 0;
-  // std::priority_queue<double, std::vector<double>, std::greater<double> >q (dist,dist+nz);
+
   int change;
-  // for(i=0;i<nz;i++){
-  //   printf("%d %d %lg\n",edges[i].src,edges[i].sink,edges[i].wght);
-  // }
   for(i=0;i<M;i++){
     change = 0;
     for(int j=0;j<nz;j++){
