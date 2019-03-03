dc3b1879ff386568ee5ee10c6fbe702b62577793
awadhesh14 <dapst8@gmail.com>
2019-03-03 22:01:51 +0530

    should run now

--- assignment-2/Parallel-SSSP/topology_driven.h
+++ assignment-2/Parallel-SSSP/topology_driven.h
@@ -5,12 +5,18 @@ __global__ void sssp_kernel_topology
 
   int index = blockIdx.x * blockDim.x + threadIdx.x,v;
   if(index>=0 && index<M){
+    printf("index %d\n",index);
+    for(int j=0;j<M;j++)
+      printf("%f ",d_dist[j] );
+    printf("\n");
     if(d_dist[index] != FLT_MAX){
       for(int i=d_sources[index]; i<d_sources[index+1]; i++){
         v = d_sinks[i];
+        printf("1\n");
         if(d_dist[index] + d_weights[i] < d_dist[v]){
           d_dist[v] = d_dist[index] + d_weights[i] ;
           *d_change = true;
+          printf("kernal v=%d change %d\n",v,*d_change);
         }
       }
     }
@@ -46,28 +52,31 @@ void topology_driven(int M, int nz, int* sources, int* sinks, float* weights){
 
     for(int i=0;i<M;i++) //initially setting all distances to INFINITY
       dist[i] = FLT_MAX;
+    dist[sssp_sources[s]] = 0; //setting the distance of source from itself to be zero
     cudaMemcpy(d_dist   , dist   , (M)   * sizeof(float), cudaMemcpyHostToDevice);
     //s = 0; //s is the single source in SSSP
-    dist[sssp_sources[s]] = 0; //setting the distance of source from itself to be zero
+
 
     bool change = true;  //to see if any relaxations have happened
     cout<<"td5"<<endl;
     while(change){
       change = false;
+      cout<<"Before Change "<<change<<endl;
       cudaMemcpy(d_change, &change, sizeof(bool), cudaMemcpyHostToDevice);
       cout<<"td6"<<endl;
       sssp_kernel_topology<<<(M + (THREADS_PER_BLOCK-1)) / THREADS_PER_BLOCK, THREADS_PER_BLOCK >>>
                         (M, nz, d_change, d_sources, d_sinks, d_weights, d_dist);
       cout<<"td7"<<endl;
       cudaMemcpy(&change, d_change, sizeof(bool), cudaMemcpyDeviceToHost);
-
-      clock_gettime(CLOCK_MONOTONIC, &tend);
-
-      float t = ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) -
-                ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec);
-      printtofile("../results/distances_topology",sssp_sources[s],dist,M);
-      printtimetofile("../times/times_topology",sssp_sources[s],t);
+      cout<<"After Change "<<change<<endl;
     }
+    cudaMemcpy(dist,d_dist, (M)   * sizeof(float),cudaMemcpyDeviceToHost);
+    clock_gettime(CLOCK_MONOTONIC, &tend);
+
+    float t = ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) -
+              ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec);
+    printtofile("../results/distances_topology",sssp_sources[s],dist,M);
+    printtimetofile("../times/times_topology",sssp_sources[s],t);
     cout<<"td8"<<endl;
   }
 
