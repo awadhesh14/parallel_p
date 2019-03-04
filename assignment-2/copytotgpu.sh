3b2ac4dc064d4b9d835024f43980d4ef12816af5
awadhesh14 <dapst8@gmail.com>
2019-03-04 13:01:00 +0530

    sent useless files to useless folder

--- assignment-2/copytotgpu.sh
+++
@@ -1,3 +0,0 @@
- scp main/* dtanwar@turing-gpu.cds.iisc.ac.in:/home/dtanwar/parallel_p/assignment-2/main/
- scp results/distances_* dtanwar@turing-gpu.cds.iisc.ac.in:/home/dtanwar/parallel_p/assignment-2/results/
- scp times/times_* dtanwar@turing-gpu.cds.iisc.ac.in:/home/dtanwar/parallel_p/assignment-2/times/
---
+++ assignment-2/useless/copytotgpu.sh
@@ -0,0 +1,3 @@
+ scp main/* dtanwar@turing-gpu.cds.iisc.ac.in:/home/dtanwar/parallel_p/assignment-2/main/
+ scp results/distances_* dtanwar@turing-gpu.cds.iisc.ac.in:/home/dtanwar/parallel_p/assignment-2/results/
+ scp times/times_* dtanwar@turing-gpu.cds.iisc.ac.in:/home/dtanwar/parallel_p/assignment-2/times/
